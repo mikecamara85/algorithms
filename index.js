73ad29a28156616b5ea6a0ab54edfbc092d90c94
Michael Camara <mikecamara85@gmail.com>
2021-12-23 14:51:27 -0500

    add Multiple Pointer pattern

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 //
 
-// 1. Frequency Counter
+// ***1. Frequency Counter***
 //
 // slow way:
 
@@ -57,6 +57,7 @@ const fastSame = (arr1, arr2) => {
 //
 
 // another fast application
+
 const validAnagram = (first, second) => {
   if (first.length !== second.length) {
     return false;
@@ -83,11 +84,70 @@ const validAnagram = (first, second) => {
   return true;
 };
 //
-console.log(validAnagram("", "")); // true
-console.log(validAnagram("aaz", "zza")); // false
-console.log(validAnagram("anagram", "nagaram")); // true
-console.log(validAnagram("rat", "car")); // false
-console.log(validAnagram("awesome", "awesom")); // false
-console.log(validAnagram("amanaplanacanalpanama", "acanalmanplanpamana")); // false
-console.log(validAnagram("qwerty", "qeywrt")); // true
-console.log(validAnagram("texttwisttime", "timetwisttext")); // true
+// console.log(validAnagram("", "")); // true
+// console.log(validAnagram("aaz", "zza")); // false
+// console.log(validAnagram("anagram", "nagaram")); // true
+// console.log(validAnagram("rat", "car")); // false
+// console.log(validAnagram("awesome", "awesom")); // false
+// console.log(validAnagram("amanaplanacanalpanama", "acanalmanplanpamana")); // false
+// console.log(validAnagram("qwerty", "qeywrt")); // true
+// console.log(validAnagram("texttwisttime", "timetwisttext")); // true
+//
+
+// ***2. Multiple Pointers***
+//
+// note: both sumZero functions here accept a PRE-SORTED array
+// slow way:
+
+const slowSumZero = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+};
+
+// console.log(slowSumZero([-4, -3, -2, -1, 0, 1, 2, 5]));
+//
+// fast way:
+
+const fastSumZero = (arr) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
+
+// console.log(fastSumZero([-4, -3, -2, -1, 0, 1, 2, 5]));
+//
+
+// another fast application:
+
+const countUniqueValues = (arr) => {
+  // here, we are allowed to modify the array
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+};
+
+console.log(countUniqueValues([1, 1, 2, 3, 3, 4, 5, 6, 6, 7]));
+//
+
+// ***3. Sliding Window Pattern***
+//
+// slow way:
